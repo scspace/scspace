5c29850ce6ac15628d198c47148eb7f54efa6f23
Yujin Kim <yujingaya@Yujinui-iMac.local>
2017-09-04 01:17:47 +0900

    2017 가을 리크루팅

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -4,34 +4,65 @@
     <div class="spacing"></div>
     <h1 class="text-center soft-red"> 학생문화공간위원회 홈페이지에 오신 것을 환영합니다. </h1>
 
-    <h2> 공지사항 </h2> <hr>
-    <table class="table table-hover">
-        <thead>
-            <tr class="hidden-xs">
-                <td> 태그 </td>
-                <td> 제목 </td>
-                <td> 날짜 </td>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            date_default_timezone_set('Asia/Seoul');
-            foreach ($notices as $notice):
-                $time_post = date_create_from_format('Y-m-d H:i:s',$notice['time_post'])->format('n월 j일 H:i');
-                ?>
-            <tr class="pointer" onclick="location.href='/notice/view/<?=$notice['id']?>'">
-                <td class="col-sm-2 hidden-xs"> <?=$notice['tag']?> </td>
-                <td class="col-sm-8 hidden-xs"> <?=$notice['title']?> </td>
-                <td class="col-sm-2 hidden-xs"> <?=$time_post?> </td>
-
-                <td class="visible-xs">
-                    <?=$notice['title']?> <br>
-                    <small>#<?=$notice['tag']?> | <?=$time_post?></small>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+    <div class="row">
+        <div class="col-sm-3">
+            <h2> 추가 리크루팅 </h2>
+            <hr>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th> 날짜 </th>
+                        <th> 일정 </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> 9월 6-7일 19-21시</td>
+                        <td> 오픈 단실 </td>
+                    </tr>
+                    <tr>
+                        <td> 9월 6-9일 </td>
+                        <td> 서류 접수 </td>
+                    </tr>
+                    <tr>
+                        <td> 9월 11-12일 </td>
+                        <td> 면접 </td>
+                    </tr>
+                </tbody>
+            </table>
+            <a href="/recruit" type="button" class="btn soft-red-button btn-lg btn-block"> 리크루팅 바로가기 </a>
+        </div>
+        <div class="col-sm-9">
+            <h2> 공지사항 </h2> <hr>
+            <table class="table table-hover">
+                <thead>
+                    <tr class="hidden-xs">
+                        <td> 태그 </td>
+                        <td> 제목 </td>
+                        <td> 날짜 </td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    date_default_timezone_set('Asia/Seoul');
+                    foreach ($notices as $notice):
+                        $time_post = date_create_from_format('Y-m-d H:i:s',$notice['time_post'])->format('n월 j일 H:i');
+                        ?>
+                    <tr class="pointer" onclick="location.href='/notice/view/<?=$notice['id']?>'">
+                        <td class="col-sm-2 hidden-xs"> <?=$notice['tag']?> </td>
+                        <td class="col-sm-8 hidden-xs"> <?=$notice['title']?> </td>
+                        <td class="col-sm-2 hidden-xs"> <?=$time_post?> </td>
+     
+                        <td class="visible-xs">
+                            <?=$notice['title']?> <br>
+                            <small>#<?=$notice['tag']?> | <?=$time_post?></small>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
 
 </main>
