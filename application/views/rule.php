30b01b2dfb56a3118edb95ebb60826cbf8d93d4d
이준희 <robot010700@gmail.com>
2018-05-18 22:25:31 +0900

    회칙 페이지 테스트

---
+++ application/views/rule.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-default navbar-static-top local-nav">
+    <div class="container">
+        <ul class="nav navbar-nav local-domain">
+            <li><a href="/rule"> 회칙 </a></li>
+        </ul>
+
+    </div>
+    <div class="container">
+        <hr style="margin:0;color:#353535;">
+    </div>
+</nav>
+<div class="affix-fix"></div>
+<style media="screen">
+    body {
+        font-size: 16px;
+    }
+    p {
+        font-size: 16px;
+    }
+
+    .faq h3 {
+        margin: 20px 0 1em;
+    }
+
+    .faq p {
+        margin-bottom: 2em;
+    }
+
+    main a:hover {
+        color: #F27575 !important;
+    }
+</style>
+<main class="container">
+
+    <h1> 회칙은 여기에 올려볼까 </h1>
+
+</main>
