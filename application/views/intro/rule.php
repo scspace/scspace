fb00820dcd1f3470cd09e0b53bcc2cd015aac215
이준희 <robot010700@gmail.com>
2018-05-20 00:01:48 +0900

    rules 위치 변경2

--- application/views/intro/rule.php
+++ application/views/intro/rule.php
@@ -29,22 +29,22 @@
         <h1 class="col-sm-3 soft-red"> 회칙 위계</h1>
         <div class="col-sm-9 margin-heading">
             <ul>
-              <li> <a href="/upload/rules/(160331) 학생문화공간위원회 회칙_최종본.pdf" download="(160331) 학생문화공간위원회 회칙_최종본.pdf"> 학생문화공간위원회 회칙</a>
+              <li> <a href="/static/documents/rules/(160331) 학생문화공간위원회 회칙_최종본.pdf" download="(160331) 학생문화공간위원회 회칙_최종본.pdf"> 학생문화공간위원회 회칙</a>
                 <ul>
                   <li> 공간 운영 세칙
                     <ol>
-                      <li> <a href="/upload/rules/(170426) 게시물 관리 규정.pdf" download="(170426) 게시물 관리 규정.pdf"> 게시물 관리 규칙</a></li>
-                      <li> <a href="/upload/rules/160331 세미나실 사용 관리 수칙.pdf" download="160331 세미나실 사용 관리 수칙.pdf"> 세미나실 운영 규칙</a></li>
-                      <li> <a href="/upload/rules/160331 창작공방 사용 관리 수칙.pdf" download="160331 창작공방 사용 관리 수칙.pdf"> 창작공방 운영 규칙</a></li>
-                      <li> <a href="/upload/rules/160331 책다방 관리이용수칙.pdf" download="160331 책다방 관리이용수칙.pdf"> 책다방 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/(170426) 게시물 관리 규정.pdf" download="(170426) 게시물 관리 규정.pdf"> 게시물 관리 규칙</a></li>
+                      <li> <a href="/static/documents/rules/160331 세미나실 사용 관리 수칙.pdf" download="160331 세미나실 사용 관리 수칙.pdf"> 세미나실 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/160331 창작공방 사용 관리 수칙.pdf" download="160331 창작공방 사용 관리 수칙.pdf"> 창작공방 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/160331 책다방 관리이용수칙.pdf" download="160331 책다방 관리이용수칙.pdf"> 책다방 운영 규칙</a></li>
                       <li> 합주실 운영 규칙</li>
-                      <li> <a href="/upload/rules/160331 다용도실 사용 관리 수칙.pdf" download="160331 다용도실 사용 관리 수칙.pdf"> 다용도실 운영 규칙</a></li>
-                      <li> <a href="/upload/rules/160331 무예실 사용 관리 수칙.pdf" download="160331 무예실 사용 관리 수칙.pdf"> 무예실 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/160331 다용도실 사용 관리 수칙.pdf" download="160331 다용도실 사용 관리 수칙.pdf"> 다용도실 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/160331 무예실 사용 관리 수칙.pdf" download="160331 무예실 사용 관리 수칙.pdf"> 무예실 운영 규칙</a></li>
                       <li> 개인연습실 운영 규칙</li>
                       <li> 피아노실 운영  규칙</li>
-                      <li> <a href="/upload/rules/160331 울림홀 사용 관리 수칙.pdf" download="160331 울림홀 사용 관리 수칙.pdf"> 울림홀 운영 규칙</a></li>
-                      <li> <a href="/upload/rules/160331 미래홀 사용 관리 수칙.pdf" download="160331 미래홀 사용 관리 수칙.pdf"> 미래홀 운영 규칙</a></li>
-                      <li> <a href="/upload/rules/(170426) 오픈스페이스 사용 규칙.pdf" download="(170426) 오픈스페이스 사용 규칙.pdf"> 오픈스페이스 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/160331 울림홀 사용 관리 수칙.pdf" download="160331 울림홀 사용 관리 수칙.pdf"> 울림홀 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/160331 미래홀 사용 관리 수칙.pdf" download="160331 미래홀 사용 관리 수칙.pdf"> 미래홀 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/(170426) 오픈스페이스 사용 규칙.pdf" download="(170426) 오픈스페이스 사용 규칙.pdf"> 오픈스페이스 운영 규칙</a></li>
                       <li> 단체실 운영 규칙</li>
                       <li> 신학관 안전점검 규칙
                         <ul>
@@ -64,7 +64,7 @@
                   </li>
                 </ul>
               </li>
-              <li> <a href="/upload/rules/(140612) 장영신 학생회관 운영준칙.pdf" download="(140612) 장영신 학생회관 운영준칙.pdf"> 장영신 학생회관 운영준칙</a></li>
+              <li> <a href="/static/documents/rules/(140612) 장영신 학생회관 운영준칙.pdf" download="(140612) 장영신 학생회관 운영준칙.pdf"> 장영신 학생회관 운영준칙</a></li>
             </ul>
         </div>
       </section>
