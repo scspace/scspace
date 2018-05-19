955ea7774d519f9c484bc94b1304245708b760dc
이준희 <robot010700@gmail.com>
2018-05-19 16:06:28 +0900

    회칙 페이지 내용 추가

--- application/views/intro/rule.php
+++ application/views/intro/rule.php
@@ -14,28 +14,62 @@
     </div>
 </nav>
 <div class="affix-fix"></div>
-<style media="screen">
-    body {
-        font-size: 16px;
-    }
-    p {
-        font-size: 16px;
-    }
 
-    .faq h3 {
-        margin: 20px 0 1em;
-    }
-
-    .faq p {
-        margin-bottom: 2em;
-    }
-
-    main a:hover {
-        color: #F27575 !important;
-    }
-</style>
 <main class="container">
+  <article>
+      <section class="row h1-block">
+          <h1 class="col-sm-3 soft-red"> 회칙</h1>
+          <div class="col-sm-9 margin-heading">
+              <p> 학생문화공간위원회의 모든 사업은 아래의 회칙/세칙을 중심으로 진행됩니다. </p>
+              <p> 각 회칙/세칙의 이름을 클릭하시면 해당하는 회칙/세칙을 다운로드할 수 있습니다. </p>
+          </div>
+      </section>
+      <hr>
+      <section class="row h1-block">
+        <h1 class="col-sm-3 soft-red"> 회칙 위계</h1>
+        <div class="col-sm-9 margin-heading">
+            <ul>
+              <li> 학생문화공간위원회 회칙
+                <ul>
+                  <li> 공간 운영 세칙
+                    <ol>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 게시물 관리 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 세미나실 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 창작공방 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 책다방 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 합주실 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 다용도실 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 무예실 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 개인연습실 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 피아노실 운영  규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 울림홀 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 미래홀 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 오픈스페이스 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 단체실 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 신학관 안전점검 규칙
+                        <ul>
+                          <li href="/upload/rules/.pdf" download=".pdf"> 단체실</li>
+                          <li href="/upload/rules/.pdf" download=".pdf"> 나머지 공간</li>
+                        </ul>
+                      </li>
+                      <li> </li>
+                    </ol>
+                  </li>
+                  <li> 사업 운영 세칙
+                    <ol>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 사무실 창고 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> CCTV 열람 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 물품 대여사업 운영 규칙</li>
+                      <li href="/upload/rules/.pdf" download=".pdf"> 유실물 관리 규칙</li>
+                    </ol>
+                  </li>
+                </ul>
+              </li>
+              <li href="/upload/rules/.pdf" download=".pdf"> 장영신 학생회관 운영준칙 </li>
+            </ul>
+        </div>
+      </section>
 
-    <h1> 회칙은 여기에 올려볼까 </h1>
 
+  </article>
 </main>
