54ec322371600006e0167d4116bc12419c459808
이준희 <robot010700@gmail.com>
2018-10-02 17:27:04 +0900

    파일 삭제

--- application/views/i18ntest.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-        <title><?php echo $this->lang->line('site_name'); ?></title>
-    </head>
-    <body>
-        <?php echo $this->lang->line('message'); ?>
-    </body>
-</html>
