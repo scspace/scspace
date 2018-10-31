1d550825c6d6b5219addd3f3bdb28ad669fcaee7
이준희 <robot010700@gmail.com>
2018-10-31 16:19:45 +0900

    welcome으로 리다이렉트

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -70,7 +70,7 @@ class Login extends CI_Controller {
 
 		$this->session->set_userdata($session);
 		$this->load->helper('url');
-		redirect($_SESSION['referer']);
+		redirect('/welcome');
 	}
 
 	public function logout(){
