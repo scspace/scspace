ba5bc46d4ecdd06d51e3e57d4fc6265295ab28f9
LAPTOP-CHSKEP79\이수범 <jake.lee.sky@gmail.com>
2018-11-19 20:45:59 +0900

    사설 수리 페이지 뼈대

---
+++ application/views/repair.php
@@ -0,0 +1,341 @@
+<nav class="navbar navbar-default navbar-static-top local-nav">
+    <div class="container">
+        <ul class="nav navbar-nav local-domain">
+            <li><a href="/repair"> 사설 수리 </a></li>
+        </ul>
+        <!--
+        <ul class="nav navbar-nav navbar-right">
+            <li>
+
+                <a href="https://goo.gl/forms/Fo5WxOBUC6iu5rzl1" style="padding:0;margin:0">
+                    <button type="button" class="btn btn-primary nav-soft-red-button">
+                        지원서 바로가기
+                    </button>
+                </a>
+
+            </li>
+        </ul> -->
+    </div>
+    <div class="container">
+        <hr style="margin:0;color:#353535;">
+    </div>
+</nav>
+<div class="affix-fix"></div>
+<style media="screen">
+    body {
+        font-size: 16px;
+    }
+    p {
+        font-size: 16px;
+    }
+
+    .faq h3 {
+        margin: 20px 0 1em;
+    }
+
+    .faq p {
+        margin-bottom: 2em;
+    }
+
+    main a:hover {
+        color: #F27575 !important;
+    }
+</style>
+<main class="container">
+
+    <h1> 사설 수리 페이지 제작 중 </h1>
+
+    <!-- <img src="/static/images/recruit-1.jpg" alt="신학관" class="img-responsive">
+    <div class="row">
+        <div class="col-sm-3">
+            <h2 class="soft-red"> 개요 </h2>
+        </div>
+        <div class="col-sm-9">
+            <h2> 카이스트 학생 문화를 이끌어갈 공간관리자를 모집합니다! </h2>
+        </div>
+        <div class="col-sm-offset-3 col-sm-9">
+            <div class="spacing"></div>
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th> 모집 대상 </td>
+                        <td colspan="3"> 4학기 동안 활동 가능한 카이스트 학부생 </td>
+                    </tr>
+                    <tr>
+                        <th> 지원 분야 </td>
+                        <td colspan="3"> 기획, 관리, 책다방, 사업지원, 미디어, 개발 </td>
+                    </tr>
+                    <tr>
+                        <th> 지원 방법 </td>
+                        <td colspan="3">
+                            <span>
+                                <a  href="https://goo.gl/forms/Fo5WxOBUC6iu5rzl1"
+                                    class="soft-red"
+                                    >지원서</a>를 3월 11일까지 제출
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th rowspan="4"> 일정 </th>
+                        <td> 오픈 공간위 </td>
+                        <td colspan="2">
+                            2월 26일 - 3월 2일, 3월 5일 - 9일 19-21시
+                            공간위실
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> 지원 마감 </td>
+                        <td colspan="2">
+                            3월 11일
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> 면접 </td>
+                        <td colspan="2">
+                            3월 12일 - 3월 14일
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td> 합격 발표 </td>
+                        <td colspan="2">
+                            3월 15일
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+
+    <hr>
+
+    <div class="row">
+        <div class="col-sm-3">
+            <h2 class="soft-red"> 공간위란? </h2>
+        </div>
+        <div class="col-sm-9">
+            <div class="spacing"></div>
+            <img src="/static/images/recruit-2.jpg" alt="공간위원들" class="img-responsive">
+            <div class="spacing"></div>
+            <p>
+                학생문화공간위원회는 카이스트의 학생 문화 공간을 운영합니다. 장영신 학생회관의 건립부터 운영에 학생들의 의견을 수렴하고 반영해오고 있습니다. 공간 활용 방안을 제시하고 공간을 개선하기도 합니다. 이외에도 다양한 학생 문화 활동을 지원하고 있습니다.
+            </p>
+            <p>
+                책을 읽고 세미나를 하거나 돈을 내지 않고도 편히 쉬어 갈 수 있는, 책다방을 운영하고 있기도 합니다.
+            </p>
+            <p>
+                물론, 일만 하는 것은 아닙니다. 동아리처럼 딸기 파티도 하고, 밥도 자주 먹으러 다니고, 공간위실에서 야식도 먹습니다. 같이 문화공간을 답사하거나 놀러 가기도 합니다.
+            </p>
+            <p>
+                공간위는 자유롭고 창의적인 분위기를 지향합니다. 지원 분야에 여러분이 하고 싶은 것이 없어도 걱정하지 마세요. 여러분이 주도적으로 제안할 수 있습니다.
+            </p>
+        </div>
+
+    </div>
+
+    <hr>
+
+    <div class="row">
+
+        <div class="col-sm-3">
+            <h2 class="soft-red"> 지원 <br class="hidden-xs"> 분야 </h2>
+        </div>
+
+        <div class="col-sm-9">
+            <div class="spacing"></div>
+            <img src="/static/images/recruit-3.jpg" alt="지원분야" class="img-responsive">
+            <h3 class="soft-red"> 기획팀 </h3>
+            <p>
+                기획팀은 오픈스페이스의 새로운 일들을 시도해보고, 각 공간의 가치를 창출해 내는 팀입니다. 기본적인 오픈스페이스 예약 및 관리업무와 함께, 오픈스페이스 개선방안, 오픈스페이스 활성화사업을 주관합니다. 또한 창작공방 관리도 맡고있습니다. 신학관을 활기차게 만들어주실 열정과 행동력이 있으시다면 꼭 넣어주세요. :)
+            </p>
+
+            <h3 class="soft-red"> 관리팀 </h3>
+            <p>
+                관리팀은 미래홀과 울림홀, 무예실, 합주실 같이 학우들이 많이 쓰는 공간을 담당합니다. 많은 학우들이 공간을 쓸 수 있도록 예약 제도를 운영하고, 이용자가 몰리는 기간에는 추첨을 진행하는 등 모두가 공평하게 학생회관을 이용할 수 있도록 관리팀은 오늘도 노력하고 있습니다. 같이 관리팀에서 일할 분들을 환영합니다!
+
+            </p>
+
+            <h3 class="soft-red"> 책다방팀 </h3>
+            <p>
+                책다방 팀은 장영신학생회관 2층에 있는 책다방을 전체적으로 관리하며, 책다방이라는 공간을 이용한 행사를 진행하는 팀입니다. 평소 책을 좋아하여 즐기거나, 문화행사를 주관해 보고 싶은 분은 부디 지원해 주시기 바랍니다.
+            </p>
+
+            <h3 class="soft-red"> 사업지원팀 </h3>
+            <p>
+            사업지원팀은 공간위가 100% 효율을 내는 데 필요한 팀입니다. 회계를 비롯해 대학우 사업 운영, 상근 관리, 업무 진행과정 단순화 등 단체 운영의 효율을 최대한 끌어올리는 역할을 하고 있습니다. 또한 공간위 내 다양한 친목 사업도 기획합니다. 올해 사업지원팀은 기존에 진행해왔던 업무들을 다각도에서 해석하여 새로운 방식으로 바꾸어 내는데 목표를 두고 사업들을 진행해왔습니다. 사업지원팀에 많이 지원해주세요 😄
+            </p>
+
+            <h3 class="soft-red"> 미디어팀 </h3>
+            <p>
+                미디어팀은 공간위의 활동 중에서 디자인에 관한 업무(공간 디자인, 단체의류 구매)와 홍보 관련 업무(포스터 제작, 공간위 페이스북 페이지 컨텐츠 제작)를 맡고 있는 팀입니다. 디자인에 관심이 있으신 분들이라면 환영합니다~
+            </p>
+
+            <h3 class="soft-red"> 개발팀 </h3>
+            <p>
+                예약 시스템을 만들고 운영합니다. 기계가 잘 하는 일을 기계에게 맡겨서, 다른 사람들이 더 의미있는 일에 집중할 수 있게 합니다. 매일 수백명이 접속하는 사이트를 운영하고 다른 사람을 편하게 하는데에 즐거움을 느낄 수 있다면, 개발팀에 지원하세요.
+            </p>
+
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="row">
+        <div class="col-sm-3">
+            <h2 class="soft-red"> 자주 <br class="hidden-xs"> 물어보는 <br class="hidden-xs"> 질문 </h2>
+        </div>
+        <div class="col-sm-9 faq">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h3> 지원 </h3>
+                </div>
+                <div class="col-sm-9">
+                    <div style="padding:6px"></div>
+                    <h4 class="soft-red">
+                        학년 제한이 있나요?
+                    </h4>
+                    <p>
+                        없지만, 2년간 활동이 가능해야합니다
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-3">
+                    <h3> 서류 심사 </h3>
+                </div>
+                <div class="col-sm-9">
+                    <div style="padding:6px"></div>
+                    <h4 class="soft-red">
+                        보유 스킬이 아무것도 없으면 어떡하죠?
+                    </h4>
+                    <p>
+                        괜찮습니다! 보유 스킬은 보다 전문적인 활동이 가능한 지원자를 보고자 하는 것이니 부담 가지지 않으셔도 됩니다.
+                    </p>
+
+                    <h4 class="soft-red">
+                        지원서 이외에 제출해야 하는 서류가 있나요?
+                    </h4>
+                    <p>
+                        미디어팀 지원자의 경우, 만약 본인의 작품이 있다면 작품을 5개 이내로 정리하여 지원서와 같이 메일로 보내주세요.
+                        개발팀 지원자의 경우, 만약 본인의 개발 자료가 있다면 정리하여 지원서와 같이 메일로 보내주세요.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-3">
+                    <h3> 면접 </h3>
+                </div>
+                <div class="col-sm-9">
+                    <div style="padding:6px"></div>
+                    <h4 class="soft-red">
+                        면접은 얼마나 오래 걸리나요?
+                    </h4>
+                    <p>
+                        20분동안 봅니다
+                    </p>
+
+                    <h4 class="soft-red">
+                        정해진 시간에 면접이 어려울 것 같은데, 어떡하죠?
+                    </h4>
+                    <p>
+                        지원서의 ‘기타’란에 가능한 시간을 적어주시면 내부 조정 후 면접 시간을 공지해 드립니다.
+                    </p>
+
+                    <h4 class="soft-red">
+                        면접은 어떤 방식으로 진행되나요? 어떤 걸 준비하면 되죠?
+                    </h4>
+                    <p>
+                        면접관 2-3명과 20분간 질의응답하는 시간을 가집니다. 충분히 답변할 수 있는 질문들로 준비했습니다. 지원서 한번 읽고 편하게 오시면 됩니다 :)
+                    </p>
+
+                    <h4 class="soft-red">
+                        장기자랑을 준비해야 하나요?
+                    </h4>
+                    <p>
+                        아니오. 장기자랑이나 부담스러운 질문은 없습니다.
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-sm-3">
+                    <h3> 합격 후 </h3>
+                </div>
+                <div class="col-sm-9">
+                    <div style="padding:6px"></div>
+                    <h4 class="soft-red">
+                        공간위 활동으로 무엇을 얻을 수 있죠?
+                    </h4>
+                    <p>
+                        일단 재미입니다! 관심있는 일을 하면서 꿈꿔왔던 공간을 실현할 수 있습니다. 비슷한, 또는 완전히 다른 관심의 사람들을 만나는 재미도 있습니다. 다른 학생들을 위한 프로그램을 기획하면서 느끼는 보람과, 강의실에서는 절대 할 수 없는 경험. 덤으로 리더십 마일리지까지 얻을 수 있습니다.
+                    </p>
+
+                    <h4 class="soft-red">
+                        활동이 빡센가요?
+                    </h4>
+                    <p>
+                        개인마다 다르지만, 대체로 힘들지 않습니다. 많은 위원들이 다른 단체 활동과 위원회 활동을 병행하고 있습니다.
+                    </p>
+
+                    <h4 class="soft-red">
+                        술을 많이 먹나요?
+                    </h4>
+                    <p>
+                        강압적으로 술을 권하지는 않습니다. 술 싫어하시면 같이 밥 먹고, 좋아하시면 같이 술 마셔요!
+                    </p>
+
+                    <h4 class="soft-red">
+                        다른 동아리와 병행이 가능할까요?
+                    </h4>
+                    <p>
+                        상근 업무나 팀별 모임 등은 개인 일정에 맞게 조정이 가능합니다. 다만 매주 수요일 밤 10시에는 전체회의가 진행되므로, 이 때 다른 동아리 활동이 있다면 위원회 활동이 조금 어려울 수 있습니다.
+                    </p>
+
+                    <h4 class="soft-red">
+                        방학 때는 뭘 하나요?
+                    </h4>
+                    <p>
+                        방학에 남을지 말지는 자유입니다. 만약 방학에 학교에 있을 경우 상근 업무를 하게 됩니다.
+                    </p>
+
+                    <h4 class="soft-red">
+                        LT가 뭐죠?
+                    </h4>
+                    <p>
+                        LT는 Leadership Training의 약자로, MT를 겸한 공간위원 연수라고 생각하시면 됩니다. 올해는 LT에서 간단한 위원회 소개 이후 친목을 다지는 시간을 가질 예정입니다.
+                    </p>
+
+                    <h4 class="soft-red">
+                        책다방에서 일하는 것에 관심이 있는데, 책다방팀만 책다방에서 근무할 수 있나요?
+                    </h4>
+                    <p>
+                        책다방 상근은 공간위 위원 모두가 진행합니다!
+                    </p>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <h3> 더 궁금한건... </h3>
+                </div>
+                <div class="col-sm-9">
+                    <div style="padding:9px"></div>
+                    <p>
+                        더 궁금한건 <span class="soft-red">오픈공간위</span>에 놀러오셔서 마음껏 질문해주셔도 되고,
+                        전화(<a href="tel:042-350-0386" class="soft-red">042-350-0386</a>)나 <a href="http://fb.com/scspace.kaist" class="soft-red">페이스북 메신저</a>를 통해 연락주셔도 됩니다.
+                        원하는 방법으로 자유롭게 물어보세요!
+                    </p>
+                </div>
+            </div>
+
+
+
+        </div>
+
+    </div> -->
+
+</main>
