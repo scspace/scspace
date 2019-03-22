4e7601856504e1c574bae3795cea65016700ffef
Akais24 <akqq24@gmail.com>
2019-03-23 00:03:36 +0900

    회칙 업데이트 및 회칙 static 폴더 정리

--- application/views/intro/rule.php
+++ application/views/intro/rule.php
@@ -29,22 +29,22 @@
         <h1 class="col-sm-3 soft-red"> 회칙 위계</h1>
         <div class="col-sm-9 margin-heading">
             <ul>
-              <li> <a href="/static/documents/rules/(160331) 학생문화공간위원회 회칙_최종본.pdf" download="(160331) 학생문화공간위원회 회칙_최종본.pdf"> 학생문화공간위원회 회칙</a>
+              <li> <a href="/static/documents/rules/001. KAIST 학생문화공간위원회 회칙 (190320).pdf" download="KAIST 학생문화공간위원회 회칙 (190320).pdf"> 학생문화공간위원회 회칙</a>
                 <ul>
-                  <li> 공간 운영 세칙
+                  <li> 공간 운영 세칙<a href="/static/documents/rules/101. 공간 운영 세칙(190320).pdf" download="공간 운영 세칙(190320).pdf"> 게시물 관리 규칙</a>
                     <ol>
-                      <li> <a href="/static/documents/rules/(170426) 게시물 관리 규정.pdf" download="(170426) 게시물 관리 규정.pdf"> 게시물 관리 규칙</a></li>
-                      <li> <a href="/static/documents/rules/(180502) 세미나실 사용 관리 수칙.pdf" download="(180502) 세미나실 사용 관리 수칙.pdf"> 세미나실 운영 규칙</a></li>
-                      <li> <a href="/static/documents/rules/160331 창작공방 사용 관리 수칙.pdf" download="160331 창작공방 사용 관리 수칙.pdf"> 창작공방 운영 규칙</a></li>
-                      <li> <a href="/static/documents/rules/160331 책다방 관리이용수칙.pdf" download="160331 책다방 관리이용수칙.pdf"> 책다방 운영 규칙</a></li>
-                      <li> <a href="/static/documents/rules/(180502) 합주실 사용 관리 수칙.pdf" download="(180502) 합주실 사용 관리 수칙.pdf">합주실 운영 규칙</li>
-                      <li> <a href="/static/documents/rules/(180502) 다용도실 사용 관리 규칙.pdf" download="(180502) 다용도실 사용 관리 규칙.pdf"> 다용도실 운영 규칙</a></li>
-                      <li> <a href="/static/documents/rules/(190307) 무예실 사용 관리 수칙.pdf" download="(190307) 무예실 사용 관리 수칙.pdf"> 무예실 운영 규칙</a></li>
-                      <li> <a href="/static/documents/rules/(180530) 개인연습실 사용 관리 규칙.pdf" download="(180530) 개인연습실 사용 관리 규칙.pdf">개인연습실 운영 규칙</li>
-                      <li> <a href="/static/documents/rules/(180530) 피아노실 사용 관리 규칙.pdf" download="(180530) 피아노실 사용 관리 규칙.pdf">피아노실 운영  규칙</li>
-                      <li> <a href="/static/documents/rules/(180502) 울림홀 사용 관리 수칙.pdf" download="(180502) 울림홀 사용 관리 수칙.pdf"> 울림홀 운영 규칙</a></li>
-                      <li> <a href="/static/documents/rules/(180502) 미래홀 사용 관리 수칙.pdf" download="(180502) 미래홀 사용 관리 수칙.pdf"> 미래홀 운영 규칙</a></li>
-                      <li> <a href="/static/documents/rules/(170426) 오픈스페이스 사용 규칙.pdf" download="(170426) 오픈스페이스 사용 규칙.pdf"> 오픈스페이스 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/102. 게시물 관리 규정 (170426).pdf" download="[학생문화공간위원회] 게시물 관리 규정 (170426).pdf"> 게시물 관리 규칙</a></li>
+                      <li> <a href="/static/documents/rules/103. 세미나실 사용 관리 수칙 (180502).pdf" download="[학생문화공간위원회] 세미나실 사용 관리 수칙 (180502).pdf"> 세미나실 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/104. 창작공방 사용 관리 수칙 (160331).pdf" download="[학생문화공간위원회] 창작공방 사용 관리 수칙 (160331).pdf"> 창작공방 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/105. 책다방 관리이용수칙 (160331).pdf" download="[학생문화공간위원회] 책다방 관리이용수칙 (160331).pdf"> 책다방 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/106. 합주실 사용 관리 수칙(180502).pdf" download="[학생문화공간위원회] 합주실 사용 관리 수칙(180502).pdf">합주실 운영 규칙</li>
+                      <li> <a href="/static/documents/rules/107. 다용도실 사용 관리 규칙(180502).pdf" download="[학생문화공간위원회] 다용도실 사용 관리 규칙(180502).pdf"> 다용도실 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/108. 무예실 사용 관리 수칙(190307).pdf" download="[학생문화공간위원회] 무예실 사용 관리 수칙(190307).pdf"> 무예실 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/109. 개인연습실 사용 관리 규칙 (180530).pdf" download="[학생문화공간위원회] 개인연습실 사용 관리 규칙 (180530).pdf">개인연습실 운영 규칙</li>
+                      <li> <a href="/static/documents/rules/110. 피아노실 사용 관리 규칙 (180530).pdf" download="[학생문화공간위원회] 피아노실 사용 관리 규칙 (180530).pdf">피아노실 운영  규칙</li>
+                      <li> <a href="/static/documents/rules/111. 울림홀 사용 관리 수칙 (180502).pdf" download="[학생문화공간위원회] 울림홀 사용 관리 수칙 (180502).pdf"> 울림홀 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/112. 미래홀 사용 관리 수칙 (180502).pdf" download="[학생문화공간위원회] 미래홀 사용 관리 수칙 (180502).pdf"> 미래홀 운영 규칙</a></li>
+                      <li> <a href="/static/documents/rules/113. 오픈스페이스 사용 규칙 (190320).pdf" download="[학생문화공간위원회] 오픈스페이스 사용 규칙 (190320).pdf"> 오픈스페이스 운영 규칙</a></li>
                       <li> 단체실 운영 규칙</li>
                       <li> 신학관 안전점검 규칙
                         <ul>
@@ -52,19 +52,20 @@
                           <li> 나머지 공간</li>
                         </ul>
                       </li>
+                      <li> <a href="/static/documents/rules/114. 예약 판단 기준(190320).pdf" download="[학생문화공간위원회] 예약 판단 기준(190320).pdf"> 예약 판단 기준</a></li>
                     </ol>
                   </li>
                   <li> 사업 운영 세칙
                     <ol>
                       <li> 사무실 창고 운영 규칙</li>
                       <li> CCTV 열람 운영 규칙</li>
-                      <li> <a href="/static/documents/rules/(180524) 물품대여 규칙.pdf" download="(180524) 물품대여 규칙.pdf"> 물품 대여사업 운영 규칙</li>
+                      <li> <a href="/static/documents/rules/201. 물품대여 규칙 (180524).pdf" download="[학생문화공간위원회] 물품대여 규칙 (180524).pdf"> 물품 대여사업 운영 규칙</li>
                       <li> 유실물 관리 규칙</li>
                     </ol>
                   </li>
                 </ul>
               </li>
-              <li> <a href="/static/documents/rules/(140612) 장영신 학생회관 운영준칙.pdf" download="(140612) 장영신 학생회관 운영준칙.pdf"> 장영신 학생회관 운영준칙</a></li>
+              <li> <a href="/static/documents/rules/301. 장영신 학생회관 운영준칙 (140612).pdf" download="장영신 학생회관 운영준칙 (140612).pdf"> 장영신 학생회관 운영준칙</a></li>
             </ul>
         </div>
       </section>
