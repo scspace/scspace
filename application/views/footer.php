1c83c1d61c06c2d2ecbedfae02b98928a9c059b5
root <root@ip-172-31-17-139.ap-northeast-2.compute.internal>
2019-04-20 07:27:38 +0000

    원상복귀

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,7 +4,7 @@
 	<div class="container">
 		<div class="col-xs-6 col-sm-4 col-md-2">
 			<ul class="list-unstyled domain">
-				<li> 공간위 </li>
+				<li> 학생문화공간위원회 </li>
 				<li><a href="/intro/committee"> 소개 </a></li>
 				<li><a href="/intro/business"> 사업 </a></li>
 				<li><a href="/intro/rule"> 회칙 </a></li>
